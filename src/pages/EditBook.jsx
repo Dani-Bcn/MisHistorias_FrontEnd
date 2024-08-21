7aabb20caa74b49562e76ab76e0a394f8199a1a3
Dani-Bcn <nneodani@gmail.com>
2024-08-21 07:26:40 +0200

    test update for vercel

--- src/pages/EditBook.jsx
+++ src/pages/EditBook.jsx
@@ -54,7 +54,7 @@ export default function EditBook() {
   const handleDelete = (e) => {
     book.chapters.splice(e, 1);
   book && book.chapters ?editBook(book._id, book) :null
-    location.reload()
+
    
   };
 
