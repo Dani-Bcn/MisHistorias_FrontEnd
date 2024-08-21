609ddbd2e074e70b1110f54df1b3c7bda29a1d9e
Dani-Bcn <nneodani@gmail.com>
2024-08-21 18:55:16 +0200

    test update for vercel

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -41,14 +41,15 @@ export default function Profile() {
   };
 
   const removeBook = (book) => {
-    user.books.map((books, i) => {
+    console.log(book)
+   /*  user.books.map((books, i) => {
       if (books._id === books) {
         user.books.splice(i, 1);
        
       }
     });
     deleteBooks(book); 
-    location.reload();
+    location.reload(); */
   };
 
   const deleteBookLibrary = async (bookId) => {
