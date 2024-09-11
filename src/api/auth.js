58d4af6df443f2370f5e2c0ffbfe432e6abc709c
Dani-Bcn <nneodani@gmail.com>
2024-09-11 13:27:23 +0200

    test update for vercel

--- src/api/auth.js
+++ src/api/auth.js
@@ -43,7 +43,7 @@ export const loginUser = async (user) =>
   res.cookie("token", "", {
     expires: new Date(0),
   });
-  res.sendStatus(200).json({message:"Sesión cerrada"});
+  res.json({message:"Sesión cerrada"});
 };
 
 //Obtener perfil usuario
