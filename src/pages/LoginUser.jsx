c7ec95284bc600a3bf17bcd3abc6578a026004a4
Dani-Bcn <nneodani@gmail.com>
2024-09-12 09:05:44 +0200

    test update for vercel

--- src/pages/LoginUser.jsx
+++ src/pages/LoginUser.jsx
@@ -32,7 +32,7 @@ export default function LoginUser() {
             setErrorEmail(true);
             setErrorPassword(false);
           } else {
-             navigate("/profile"),     
+             navigate("/profile")
           }
         })}
       >
