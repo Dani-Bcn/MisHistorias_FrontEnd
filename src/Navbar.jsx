38f95287ed342d21c8f3a43a78a9dab5e823d735
Dani-Bcn <nneodani@gmail.com>
2024-09-12 20:26:13 +0200

    Test front-end

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -55,7 +55,7 @@ export default function Navbar() {
                         setTimeout(() => {
                         location.reload()
                           setState(!state);
-                        }, 10);
+                        }, 50);
                     }}
                   >
                     Logout
