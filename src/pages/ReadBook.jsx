7325583564600637921c548a6c69f54f6d47c8e3
Dani-Bcn <nneodani@gmail.com>
2025-04-20 11:19:54 +0200

    improve image handling

--- src/pages/ReadBook.jsx
+++ src/pages/ReadBook.jsx
@@ -36,6 +36,8 @@ export default function ReadBook() {
 
   return (
     <main className="w-screen  text-white p-5 flex flex-col  items-center justify-center gap-5 mt-10">
+      <img src={book?.imageUrl} alt={book?.dataUser.imageUserUrl} 
+      className="z-0 w-screen h-screen object-contain absolute"/>
       <header className="w-screen flex flex-col items-center gap-2 p-5">
         <h2 className="text-2xl xl:text-5xl mb-5">
           <span>{book?.dataUser.userName} </span>
@@ -62,29 +64,7 @@ export default function ReadBook() {
      
       </header>
       <div className="w-[80%] h-[0.5px] bg-gradient-to-r from-indigo-500/0 via-purple-500 to-pink-500/0"></div>
-      {/*   {book.chapters?.length > 0 && (
-        <aside className="text-red-50 lg:w-56 w-full bg-gray-800 bg-opacity-70 rounded-xl p-5 lg:fixed lg:top-20 mt-10">
-          <h3 className="text-xl border-b border-gray-500 mb-3">
-            <span>C</span>apítulos
-          </h3>
-          <ul className="flex flex-col gap-3">
-            {book.chapters.map((chapter, i) => (
-              <Link
-                key={i}
-                to={chapter.title}
-                spy
-                smooth
-                offset={-150}
-                duration={500}
-                className="cursor-pointer text-start text-sm hover:text-gray-300"
-              >
-                {chapter.title}
-              </Link>
-            ))}
-          </ul>
-        </aside>
-      )} */}
-
+     
       <div className="lg:w-2/4 w-full mx-auto text-red-50">
         {book.chapters?.map((chapter, i) => (
           <article
