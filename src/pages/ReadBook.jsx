158755d49c579aedf23b3f5043d2861adfbaf169
Dani-Bcn <nneodani@gmail.com>
2025-04-20 13:08:11 +0200

    refactor ReadBook component; clean up code formatting for improved readability

--- src/pages/ReadBook.jsx
+++ src/pages/ReadBook.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { getBook, profile } from "../api/auth";
 import { useNavigate } from "react-router-dom";
 
-
 export default function ReadBook() {
   const navigate = useNavigate();
   const [bookId] = useState(localStorage.getItem("bookId"));
@@ -32,19 +31,17 @@ export default function ReadBook() {
   }, [bookId]);
 
   if (!book) return null;
-  console.log(book)
+  console.log(book);
 
   return (
     <main className="w-screen  text-white p-5 flex flex-col  items-center justify-start gap-5 mt-10">
-     
       <header className="w-screen flex flex-col items-center gap-2 p-5">
         <h2 className="text-2xl xl:text-5xl mb-5">
           <span>{book?.dataUser.userName} </span>
           {book?.dataUser.lastName}
         </h2>
         <img
-         
-          src= {book?.dataUser.imageUserUrl}
+          src={book?.dataUser.imageUserUrl}
           alt="User"
           className="w-14 h-14 xl:w-40 xl:h-40 object-cover rounded-[100%] border-2 border-orange-400 cursor-pointer"
         />
@@ -54,16 +51,14 @@ export default function ReadBook() {
         </h3>
         <div className="w-[80%] h-[0.5px] bg-gradient-to-r from-indigo-500/0 via-purple-500 to-pink-500/0"></div>
         <div className="w-screen xl:w-1/2  p-5">
-          <h3 >
+          <h3>
             <span>Descripción :</span>
           </h3>
           <p>{book.description}</p>
-          
         </div>
-     
       </header>
       <div className="w-[80%] h-[0.5px] bg-gradient-to-r from-indigo-500/0 via-purple-500 to-pink-500/0"></div>
-     
+
       <div className="lg:w-2/4 w-full mx-auto text-red-50">
         {book.chapters?.map((chapter, i) => (
           <article
